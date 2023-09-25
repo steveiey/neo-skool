a59d26eb90d23c68e98fa1a4b1c51754ed295297
Steveiey <damientwc@gmail.com>
2023-09-25 12:48:54 -0700

    commit stage

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,8 @@
 # Damien Cheng
 # 09/21/23
 
+import random
+
 # Goal: Greet the user, talk
 
 # Greet
@@ -18,3 +20,15 @@ FOOD = input("What is ur favourite food. Dont disappoint me again")
 # Respond food
 
 print(f"AGLJLGJESIGJSLKGJSLK NO {FOOD} ALLOWEDSD ffsafsddaafeag")
+
+
+# NO MORE REPeating
+# HAVE A LIST OF COMMENTS
+
+LIST = [
+    "AMOGUS", "NO", "I hate you", "NO", "I dont like you"
+    ]
+
+randomfod = random.choice (LIST)
+
+print(randomfod)
