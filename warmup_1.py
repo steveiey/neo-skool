8490b5ef68490839e76bdf9bff8808e271676197
Steveiey <damientwc@gmail.com>
2023-10-23 12:50:26 -0700

    yep

--- warmup_1.py
+++ warmup_1.py
@@ -3,3 +3,22 @@
 # 9/25/23
 
 print("BEEP BOOP")
+
+some_string = "This is some string."
+print(some_string.upper().strip(".!").lower().split())
+
+
+is_raining = True
+is_cloudy = True
+is_sunny = True
+if is_raining:
+    if is_sunny:
+        print("Bring your shades and your umbrella!")
+    elif is_cloudy:
+        print("Brrrrr.")
+
+adjectives = ["Golden", "Tiny", "Purple"]
+stars = ["Polaris", "Vega", "Sun"]
+for adj in adjectives:
+    for word in stars:
+        print(adj + word)
