11d1754dbe0fd01583ecc87ab73d2b46129b9015
Steveiey <damientwc@gmail.com>
2023-11-16 10:43:25 -0800

    did parent bot

--- parentalbot.py
+++ parentalbot.py
@@ -2,3 +2,17 @@
 # damien Cheng
 # Nov 16
 
+questions = ["Did you eat?", "Did you study?", "Did you do your laundry?", "Did you call grandma?"]
+score = 0
+for question in questions:
+    sus = input(question)
+    if sus == "yes":
+        score = score + 1
+
+
+if score == 0:
+    print("I'm coming over.")
+elif score <= 2:
+    print("Ok.")
+elif score <= 4:
+    print("Good!")
