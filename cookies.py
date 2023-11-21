1d1adb06b527881362608c1082c626f2035d2be3
Steveiey <damientwc@gmail.com>
2023-11-21 12:48:51 -0800

    sus

---
+++ cookies.py
@@ -0,0 +1,21 @@
+
+# Cookies
+# Author: Damien Cheng
+# 21 November 2023
+
+import turtle
+
+# Make baker turtle
+baker_turtle = turtle.Turtle()
+baker_turtle.color("brown")
+
+# Draw outline of cookie
+baker_turtle.penup()
+baker_turtle.goto(-5, -30)
+baker_turtle.pendown()
+baker_turtle.circle(30)
+baker_turtle.penup()
+
+
+# Add chips
+turtle.done()
